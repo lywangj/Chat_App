4c85478570a8955377a0d0a11ca9ad989f1322f9
og21893_Ubuntu <og21893@bristol.ac.uk>
2022-11-06 14:06:06 +0000

    update tmp version for adding user info

--- client.js
+++ client.js
@@ -2,29 +2,37 @@ const socket = io('http://localhost:3000');
 const messages = document.getElementById('messages');
 const msgForm = document.getElementById('msgForm');
 
-socket.on('message', data => {
+socket.on('message', (user, data) => {
+    console.log(user)
     console.log(data)
-    appendMessages(data)
+    appendMessages(user, data)
 })
 socket.on('output-messages', data => {
     console.log(data)
     if (data.length) {
         data.forEach(message => {
-            appendMessages(message.msg)
+            // var username = "User";
+            appendMessages(message.msg, message.msg)
         });
     }
 })
 
 msgForm.addEventListener('submit', e => {
     e.preventDefault()
-    socket.emit('chatmessage', msgForm.msg.value)
-    console.log('submit from msgfrom', msgForm.msg.value)
+    socket.emit('chatmessage', msgForm.user.value, msgForm.msg.value)
+    console.log('submit from '+ msgForm.user.value + ' with ' + msgForm.msg.value)
     msgForm.msg.value = '';
-
 
 })
 
-function appendMessages(message) {
-    const html = `<div>${message}</div>`
+function appendMessages(user, message) {
+    const html = `<div>${user} + ${message}</div>`
     messages.innerHTML += html
+    // var message = document.createElement('div');
+    // message.setAttribute('class', 'chat-message');
+    // message.textContent = ${message}+": "+message;
+    // // message.textContent = data[x].name+": "+data[x].message;
+    // console.log(message.msg);
+    // messages.appendChild(message);
+    // messages.insertBefore(message, messages.firstChild);
 }
